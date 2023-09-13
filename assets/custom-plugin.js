714d0d861b9c08954705e3d5bd48aabe4053920e
andreadn <andre@agenciamobidick.com.br>
2023-09-13 16:04:23 -0300

    chages

--- assets/custom-plugin.js
+++ assets/custom-plugin.js
@@ -1,27 +1,4 @@
 
 jQuery(document).ready(function($) {
 
-	$('.kai-tab-links a').on('click', function(e) {
-    e.preventDefault();
-    var currentAttrValue = $(this).attr('href');
-
-    $('.kai-tab-content').hide();
-    $('.kai-tab-links li').removeClass('active');
-    $(this).parent('li').addClass('active');
-    $(currentAttrValue).fadeIn(300);
-
-  });
-
-	$('.kai-tabs-to-left').on('click', function() {
-		$('.kai-tabs-scroll').animate({scrollLeft: '-=300'}, 300);
-	});
-	
-	$('.kai-tabs-to-right').on('click', function() {
-		$('.kai-tabs-scroll').animate({scrollLeft: '+=300'}, 300);
-	});
-
-	setTimeout(() => {
-		jQuery('.kai-tab-links li:first-child a').click()
-	}, 600);
-
 });
